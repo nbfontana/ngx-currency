7f29ae92674a3753af877d10cd45b5f3f9a90f0e
Neri Bez Fontana <nerifontana@gmail.com>
2017-09-29 14:28:31 -0300

    feat: input field added in demo app

--- demo/demo.component.ts
+++ demo/demo.component.ts
@@ -3,13 +3,26 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'demo-app',
   template: `
-    <form class="container" [formGroup]="form">
+    <form class="container">
       <div class="row">
-        <div class="col-md-7 form-group">
+        <div class="col-md-3 col-sm-5 form-group">
+
+          <input class="form-control" maxlength="20" currencyMask [(ngModel)]="value"
+                 [placeholder]="'Teste'" [options]="ngxCurrencyOptions">
 
         </div>
       </div>
     </form>`
 })
 export class DemoComponent {
+
+  public value: number;
+
+  public ngxCurrencyOptions = {
+    prefix: 'R$ ',
+    thousands: '.',
+    decimal: ',',
+    allowNegative: false
+  };
+
 }
