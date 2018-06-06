7b968245fba7003edfe26691801abc4f9bd15ac5
Neri Bez Fontana <nerifontana@gmail.com>
2018-06-06 19:51:47 -0300

    refactor: change main testing file and delete useless tests

--- test/currency-mask-config.spec.ts
+++ test/currency-mask-config.spec.ts
@@ -2,24 +2,21 @@ import { InputService } from './../src/input.service';
 import { fakeAsync } from "@angular/core/testing";
 import { expect } from "chai";
 
-describe('***** Should be true *****', () => {
+describe('Testing CurrencyMaskConfig', () => {
   var options: any;
   var inputService: InputService;
+
   beforeEach(() => {
-    options = [
-      {
-        prefix: 'R$ ',
-        thousands: '.',
-        decimal: ',',
-        allowNegative: false,
-        nullable: false
-      }
-    ]
+    options = [{
+      prefix: 'R$ ',
+      thousands: '.',
+      decimal: ',',
+      allowNegative: false,
+      nullable: false
+    }];
+
     inputService = new InputService(null, options);
   })
-  it('Should be true', fakeAsync(() => {
-    expect(true).to.equals(true);
-  }));
 
   it('should return null because the nullable parameterization is true', fakeAsync(() => {
     var option: any = { nullable: true }
