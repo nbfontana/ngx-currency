c46c1c1039fcd9318c89ab298018108aa1230e69
Elliot Mendiola <elliot_mendiola@ultimatesoftware.com>
2018-09-11 14:43:43 -0400

    fix(forRoot): Added forRoot to the module for proper default config

--- src/currency-mask.module.ts
+++ src/currency-mask.module.ts
@@ -1,11 +1,22 @@
-import {NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {CurrencyMaskDirective} from "./currency-mask.directive";
+import {CURRENCY_MASK_CONFIG, CurrencyMaskConfig} from "./currency-mask.config";
 
 @NgModule({
   imports: [ CommonModule, FormsModule ],
   declarations: [ CurrencyMaskDirective ],
   exports: [ CurrencyMaskDirective ]
 })
-export class NgxCurrencyModule {}
+export class NgxCurrencyModule {
+  static forRoot(config: CurrencyMaskConfig): ModuleWithProviders {
+    return {
+      ngModule: NgxCurrencyModule,
+      providers: [{
+        provide: CURRENCY_MASK_CONFIG,
+        useValue: config,
+      }]
+    }
+  }
+}
