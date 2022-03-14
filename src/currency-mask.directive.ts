364b420ca0a1790dd915fcd19a7a5ea7ed3c96d9
arturovt <arthurandrosovich@gmail.com>
2022-03-14 22:51:10 +0200

    perf: do not run change detection on `drop` events and move condition out of the class

--- src/currency-mask.directive.ts
+++ src/currency-mask.directive.ts
@@ -10,7 +10,9 @@ import {
   KeyValueDiffers,
   Input,
   OnInit,
-  Optional
+  Optional,
+  OnDestroy,
+  NgZone
 } from "@angular/core";
 
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
@@ -23,12 +25,20 @@ export const CURRENCYMASKDIRECTIVE_VALUE_ACCESSOR: any = {
   multi: true,
 };
 
+// Do not store the condition on the class since it'll be executed every time the instance is created.
+const IS_CHROME_ANDROID =
+  typeof navigator !== 'undefined' &&
+  !!navigator.userAgent &&
+  /chrome/i.test(navigator.userAgent) &&
+  /android/i.test(navigator.userAgent);
+
 @Directive({
     selector: "[currencyMask]",
     providers: [CURRENCYMASKDIRECTIVE_VALUE_ACCESSOR]
 })
-export class CurrencyMaskDirective implements AfterViewInit, ControlValueAccessor, DoCheck, OnInit {
-
+export class CurrencyMaskDirective
+  implements AfterViewInit, ControlValueAccessor, DoCheck, OnInit, OnDestroy
+{
   @Input() options: Partial<CurrencyMaskConfig> = {};
 
   public inputHandler: InputHandler;
@@ -47,11 +57,16 @@ export class CurrencyMaskDirective implements AfterViewInit, ControlValueAccesso
       inputMode: CurrencyMaskInputMode.FINANCIAL
   };
 
-  constructor(@Optional() @Inject(CURRENCY_MASK_CONFIG) private currencyMaskConfig: CurrencyMaskConfig,
-                                                        private elementRef: ElementRef,
-                                                        private keyValueDiffers: KeyValueDiffers) {
+  constructor(
+    private ngZone: NgZone,
+    @Optional()
+    @Inject(CURRENCY_MASK_CONFIG)
+    currencyMaskConfig: CurrencyMaskConfig,
+    private elementRef: ElementRef<HTMLInputElement>,
+    keyValueDiffers: KeyValueDiffers
+  ) {
     if (currencyMaskConfig) {
-        this.optionsTemplate = currencyMaskConfig;
+      this.optionsTemplate = currencyMaskConfig;
     }
 
     this.keyValueDiffer = keyValueDiffers.find({}).create();
@@ -70,6 +85,11 @@ export class CurrencyMaskDirective implements AfterViewInit, ControlValueAccesso
 
   ngOnInit() {
     this.inputHandler = new InputHandler(this.elementRef.nativeElement, (<any>Object).assign({}, this.optionsTemplate, this.options));
+    this.ngZone.runOutsideAngular(() => this.elementRef.nativeElement.addEventListener('drop', preventDefault));
+  }
+
+  ngOnDestroy(): void {
+    this.elementRef.nativeElement.removeEventListener('drop', preventDefault);
   }
 
   @HostListener("blur", ["$event"])
@@ -79,48 +99,37 @@ export class CurrencyMaskDirective implements AfterViewInit, ControlValueAccesso
 
   @HostListener("cut", ["$event"])
   handleCut(event: any) {
-    if (!this.isChromeAndroid()) {
+    if (!IS_CHROME_ANDROID) {
       !this.isReadOnly() && this.inputHandler.handleCut(event);
     }
   }
 
   @HostListener("input", ["$event"])
   handleInput(event: any) {
-    if (this.isChromeAndroid()) {
+    if (IS_CHROME_ANDROID) {
       !this.isReadOnly() && this.inputHandler.handleInput(event);
     }
   }
 
   @HostListener("keydown", ["$event"])
   handleKeydown(event: any) {
-    if (!this.isChromeAndroid()) {
+    if (!IS_CHROME_ANDROID) {
       !this.isReadOnly() && this.inputHandler.handleKeydown(event);
     }
   }
 
   @HostListener("keypress", ["$event"])
   handleKeypress(event: any) {
-    if (!this.isChromeAndroid()) {
+    if (!IS_CHROME_ANDROID) {
       !this.isReadOnly() && this.inputHandler.handleKeypress(event);
     }
   }
 
   @HostListener("paste", ["$event"])
   handlePaste(event: any) {
-    if (!this.isChromeAndroid()) {
+    if (!IS_CHROME_ANDROID) {
       !this.isReadOnly() && this.inputHandler.handlePaste(event);
     }
-  }
-
-  @HostListener("drop", ["$event"])
-  handleDrop(event: any) {
-    if (!this.isChromeAndroid()) {
-      event.preventDefault();
-    }
-  }
-
-  isChromeAndroid(): boolean {
-    return /chrome/i.test(navigator.userAgent) && /android/i.test(navigator.userAgent);
   }
 
   isReadOnly(): boolean {
@@ -141,5 +150,11 @@ export class CurrencyMaskDirective implements AfterViewInit, ControlValueAccesso
 
   writeValue(value: number): void {
     this.inputHandler.setValue(value);
+  }
+}
+
+function preventDefault(event: Event): void {
+  if (!IS_CHROME_ANDROID) {
+    event.preventDefault();
   }
 }
